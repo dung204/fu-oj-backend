d1dcc193b5461103ee105c29926450c2d6673f5d
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2025-10-21 22:52:04 +0700

    refactor: groups add class implement

---
+++ src/main/java/com/example/modules/groups/services/GroupServiceImp.java
@@ -0,0 +1,199 @@
+package com.example.modules.groups.services;
+
+import com.example.modules.exercises.entities.Exercise;
+import com.example.modules.exercises.repositories.ExercisesRepository;
+import com.example.modules.groups.dtos.GroupRequestDTO;
+import com.example.modules.groups.dtos.GroupResponseDTO;
+import com.example.modules.groups.dtos.GroupUpdateRequestDTO;
+import com.example.modules.groups.entities.Group;
+import com.example.modules.groups.exeptions.GroupHasAlreadyBeenUsedException;
+import com.example.modules.groups.repositories.GroupsRepository;
+import com.example.modules.groups.utils.GroupMapper;
+import com.example.modules.users.entities.User;
+import com.example.modules.users.repositories.UsersRepository;
+import java.security.SecureRandom;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class GroupServiceImp implements GroupsService {
+
+  private static final String CHARS =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  private static final SecureRandom random = new SecureRandom();
+
+  private final GroupsRepository groupsRepository;
+  private final UsersRepository usersRepository;
+  private final GroupMapper groupMapper;
+  private final ExercisesRepository exercisesRepository;
+
+  public GroupServiceImp(
+    GroupsRepository groupsRepository,
+    UsersRepository usersRepository,
+    GroupMapper groupMapper,
+    ExercisesRepository exercisesRepository
+  ) {
+    this.groupsRepository = groupsRepository;
+    this.usersRepository = usersRepository;
+    this.groupMapper = groupMapper;
+    this.exercisesRepository = exercisesRepository;
+  }
+
+  @Override
+  @Transactional
+  public GroupResponseDTO addStudentsToGroup(String groupId, List<String> studentIds) {
+    Group group = groupsRepository.findGroupById(groupId);
+    List<User> students = usersRepository.findAllById(studentIds);
+    if (group.getStudents() == null) {
+      group.setStudents(new ArrayList<>());
+    }
+    group.getStudents().addAll(students);
+    groupsRepository.save(group);
+    return groupMapper.toGroupResponseDTO(group);
+  }
+
+  @Override
+  public List<User> getStudentsByGroupId(String groupId) {
+    Group group = groupsRepository.findGroupById(groupId);
+    List<User> students = group.getStudents();
+    if (students == null) {
+      students = new ArrayList<>();
+    }
+    return students;
+  }
+
+  @Override
+  @Transactional
+  public GroupResponseDTO removeStudentsFromGroup(String groupId, List<String> studentIds) {
+    Group group = groupsRepository.findGroupById(groupId);
+    List<User> studentsToRemove = group
+      .getStudents()
+      .stream()
+      .filter(student -> studentIds.contains(student.getId()))
+      .toList();
+
+    group.getStudents().removeAll(studentsToRemove);
+    groupsRepository.save(group);
+    return groupMapper.toGroupResponseDTO(group);
+  }
+
+  @Override
+  @Transactional
+  public GroupResponseDTO removeExercisesFromGroup(String groupId, List<String> exerciseIds) {
+    Group group = groupsRepository.findGroupById(groupId);
+    List<Exercise> exercisesToRemove = group
+      .getExercises()
+      .stream()
+      .filter(exercise -> exerciseIds.contains(exercise.getId()))
+      .toList();
+    group.getExercises().removeAll(exercisesToRemove);
+    groupsRepository.save(group);
+    return groupMapper.toGroupResponseDTO(group);
+  }
+
+  @Override
+  public List<Exercise> getExerciseByGroupId(String groupId) {
+    Group group = groupsRepository.findGroupById(groupId);
+    List<Exercise> exercises = group.getExercises();
+    if (exercises == null) {
+      exercises = new ArrayList<>();
+    }
+    return exercises;
+  }
+
+  @Override
+  @Transactional
+  public GroupResponseDTO addExerciseToGroup(String groupId, List<String> exerciseIds) {
+    Group group = groupsRepository.findGroupById(groupId);
+    List<Exercise> exercises = exercisesRepository.findAllById(exerciseIds);
+    group.getExercises().addAll(exercises);
+    groupsRepository.save(group);
+    return groupMapper.toGroupResponseDTO(group);
+  }
+
+  @Override
+  public List<GroupResponseDTO> getGroupByInstructorId(String ownerId) {
+    List<Group> groups = groupsRepository.getGroupByOwnerId(ownerId);
+    if (groups == null) {
+      groups = new ArrayList<>();
+    }
+    return groupMapper.toGroupResponseDTOList(groups);
+  }
+
+  @Override
+  public List<GroupResponseDTO> getGroups() {
+    List<Group> groups = groupsRepository.findAll();
+    if (groups == null) {
+      groups = new ArrayList<>();
+    }
+    return groupMapper.toGroupResponseDTOList(groups);
+  }
+
+  @Override
+  public GroupResponseDTO deleteGroup(String id) {
+    Group group = groupsRepository.getGroupById(id);
+    group.softDelete();
+    groupsRepository.save(group);
+    return groupMapper.toGroupResponseDTO(group);
+  }
+
+  @Override
+  public GroupResponseDTO updateGroup(GroupUpdateRequestDTO requestDTO) {
+    Group group = groupsRepository.getGroupById(requestDTO.getId());
+    group.setName(requestDTO.getName());
+    group.setDescription(requestDTO.getDescription());
+    group.setPublic(requestDTO.isPublic());
+    group.setUpdatedTimestamp(Instant.now());
+    groupsRepository.save(group);
+    return groupMapper.toGroupResponseDTO(group);
+  }
+
+  @Override
+  public GroupResponseDTO addGroup(GroupRequestDTO groupRequestDTO) {
+    final String nameGroup = groupRequestDTO.getName();
+    final Optional<Group> existingGroup = groupsRepository.existsGroupByName(nameGroup);
+    if (existingGroup.isPresent()) {
+      throw new GroupHasAlreadyBeenUsedException();
+    }
+    User instructor = usersRepository.findUserById(groupRequestDTO.getOwnerId());
+
+    Group group = Group.builder()
+      .name(groupRequestDTO.getName())
+      .description(groupRequestDTO.getDescription())
+      .isPublic(groupRequestDTO.isPublic())
+      .code(generateUniqueClassCode())
+      .instructor(instructor)
+      .build();
+    if (!groupRequestDTO.isPublic()) {
+      group.setCode("");
+    }
+
+    groupsRepository.save(group);
+    return groupMapper.toGroupResponseDTO(group);
+  }
+
+  @Override
+  @Transactional
+  public String generateUniqueClassCode() {
+    String code;
+    do {
+      code = generateCode(8);
+    } while (groupsRepository.existsGroupByCode(code));
+    return code;
+  }
+
+  @Override
+  public String generateCode(int length) {
+    StringBuilder sb = new StringBuilder(length);
+    for (int i = 0; i < length; i++) {
+      sb.append(CHARS.charAt(random.nextInt(CHARS.length())));
+    }
+    return sb.toString();
+  }
+}
--- src/main/java/com/example/modules/groups/services/GroupsService.java
+++ src/main/java/com/example/modules/groups/services/GroupsService.java
@@ -1,197 +1,36 @@
 package com.example.modules.groups.services;
 
 import com.example.modules.exercises.entities.Exercise;
-import com.example.modules.exercises.repositories.ExercisesRepository;
 import com.example.modules.groups.dtos.GroupRequestDTO;
 import com.example.modules.groups.dtos.GroupResponseDTO;
 import com.example.modules.groups.dtos.GroupUpdateRequestDTO;
-import com.example.modules.groups.entities.Group;
-import com.example.modules.groups.exeptions.GroupHasAlreadyBeenUsedException;
-import com.example.modules.groups.repositories.GroupsRepository;
-import com.example.modules.groups.utils.GroupMapper;
 import com.example.modules.users.entities.User;
-import com.example.modules.users.repositories.UsersRepository;
-import java.security.SecureRandom;
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Slf4j
-public class GroupsService {
+public interface GroupsService {
+  GroupResponseDTO addStudentsToGroup(String groupId, List<String> studentIds);
 
-  private static final String CHARS =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  private static final SecureRandom random = new SecureRandom();
-  private final GroupsRepository groupsRepository;
-  private final UsersRepository usersRepository;
-  private final GroupMapper groupMapper;
-  private final ExercisesRepository exercisesRepository;
+  List<User> getStudentsByGroupId(String groupId);
 
-  public GroupsService(
-    GroupsRepository groupsRepository,
-    UsersRepository usersRepository,
-    GroupMapper groupMapper,
-    ExercisesRepository exercisesRepository
-  ) {
-    this.groupsRepository = groupsRepository;
-    this.usersRepository = usersRepository;
-    this.groupMapper = groupMapper;
-    this.exercisesRepository = exercisesRepository;
-  }
+  GroupResponseDTO removeStudentsFromGroup(String groupId, List<String> studentIds);
 
-  @Transactional
-  public GroupResponseDTO addStudentsToGroup(String groupId, List<String> studentIds) {
-    Group group = groupsRepository.findGroupById(groupId);
-    List<User> students = usersRepository.findAllById(studentIds);
-    if (group.getStudents() == null) {
-      group.setStudents(new ArrayList<>());
-    }
-    group.getStudents().addAll(students);
-    groupsRepository.save(group);
-    return groupMapper.toGroupResponseDTO(group);
-  }
+  GroupResponseDTO removeExercisesFromGroup(String groupId, List<String> exerciseIds);
 
-  public List<User> getStudentsByGroupId(String groupId) {
-    Group group = groupsRepository.findGroupById(groupId);
-    List<User> students = group.getStudents();
-    if (students == null) {
-      students = new ArrayList<>();
-    }
-    return students;
-  }
+  List<Exercise> getExerciseByGroupId(String groupId);
 
-  @Transactional
-  public GroupResponseDTO removeStudentsFromGroup(String groupId, List<String> studentIds) {
-    Group group = groupsRepository.findGroupById(groupId);
-    List<User> studentsToRemove = group
-      .getStudents()
-      .stream()
-      .filter(student -> studentIds.contains(student.getId()))
-      .toList();
+  GroupResponseDTO addExerciseToGroup(String groupId, List<String> exerciseIds);
 
-    group.getStudents().removeAll(studentsToRemove);
-    groupsRepository.save(group);
-    return groupMapper.toGroupResponseDTO(group);
-  }
+  List<GroupResponseDTO> getGroupByInstructorId(String ownerId);
 
-  @Transactional
-  public GroupResponseDTO removeExercisesFromGroup(String groupId, List<String> exerciseIds) {
-    Group group = groupsRepository.findGroupById(groupId);
-    List<Exercise> exercisesToRemove = group
-      .getExercises()
-      .stream()
-      .filter(exercise -> exerciseIds.contains(exercise.getId()))
-      .toList();
-    group.getExercises().removeAll(exercisesToRemove);
-    groupsRepository.save(group);
-    return groupMapper.toGroupResponseDTO(group);
-  }
+  List<GroupResponseDTO> getGroups();
 
-  //get exercise by groupId
-  public List<Exercise> getExerciseByGroupId(String groupId) {
-    Group group = groupsRepository.findGroupById(groupId);
-    List<Exercise> exercises = group.getExercises();
-    if (exercises == null) {
-      exercises = new ArrayList<>();
-    }
-    return exercises;
-  }
+  GroupResponseDTO deleteGroup(String id);
 
-  //add exercise to group
-  @Transactional
-  public GroupResponseDTO addExerciseToGroup(String groupId, List<String> exerciseIds) {
-    Group group = groupsRepository.findGroupById(groupId);
-    List<Exercise> exercises = exercisesRepository.findAllById(exerciseIds);
-    group.getExercises().addAll(exercises);
-    groupsRepository.save(group);
-    return groupMapper.toGroupResponseDTO(group);
-  }
+  GroupResponseDTO updateGroup(GroupUpdateRequestDTO requestDTO);
 
-  //get group by id
-  public List<GroupResponseDTO> getGroupByInstructorId(String ownerId) {
-    List<Group> groups = groupsRepository.getGroupByOwnerId(ownerId);
-    if (groups == null) {
-      groups = new ArrayList<>();
-    }
-    return groupMapper.toGroupResponseDTOList(groups);
-  }
+  GroupResponseDTO addGroup(GroupRequestDTO groupRequestDTO);
 
-  //get all group
-  public List<GroupResponseDTO> getGroups() {
-    List<Group> groups = groupsRepository.findAll();
-    if (groups == null) {
-      groups = new ArrayList<>();
-    }
-    return groupMapper.toGroupResponseDTOList(groups);
-  }
+  String generateUniqueClassCode();
 
-  //delete group
-  public GroupResponseDTO deleteGroup(String id) {
-    Group group = groupsRepository.getGroupById(id);
-    group.softDelete();
-    groupsRepository.save(group);
-    return groupMapper.toGroupResponseDTO(group);
-  }
-
-  //update group by id group
-  public GroupResponseDTO updateGroup(GroupUpdateRequestDTO requestDTO) {
-    Group group = groupsRepository.getGroupById(requestDTO.getId());
-    group.setName(requestDTO.getName());
-    group.setDescription(requestDTO.getDescription());
-    group.setPublic(requestDTO.isPublic());
-    group.setUpdatedTimestamp(Instant.now());
-    groupsRepository.save(group);
-    return groupMapper.toGroupResponseDTO(group);
-  }
-
-  //add group
-  public GroupResponseDTO addGroup(GroupRequestDTO groupRequestDTO) {
-    final String nameGroup = groupRequestDTO.getName();
-    final Optional<Group> existingGroup = groupsRepository.existsGroupByName(nameGroup);
-    if (existingGroup.isPresent()) {
-      throw new GroupHasAlreadyBeenUsedException();
-    }
-    User instructor = usersRepository.findUserById(groupRequestDTO.getOwnerId());
-
-    //create group
-    Group group = Group.builder()
-      .name(groupRequestDTO.getName())
-      .description(groupRequestDTO.getDescription())
-      .isPublic(groupRequestDTO.isPublic())
-      .code(generateUniqueClassCode())
-      .instructor(instructor)
-      .build();
-    if (!groupRequestDTO.isPublic()) {
-      group.setCode("");
-    }
-
-    groupsRepository.save(group);
-
-    return groupMapper.toGroupResponseDTO(group);
-  }
-
-  @Transactional
-  public String generateUniqueClassCode() {
-    String code;
-    do {
-      code = generateCode(8);
-    } while (groupsRepository.existsGroupByCode(code));
-    return code;
-  }
-
-  //generate code for class
-  public String generateCode(int length) {
-    StringBuilder sb = new StringBuilder(length);
-    for (int i = 0; i < length; i++) {
-      sb.append(CHARS.charAt(random.nextInt(CHARS.length())));
-    }
-    return sb.toString();
-  }
+  String generateCode(int length);
 }
