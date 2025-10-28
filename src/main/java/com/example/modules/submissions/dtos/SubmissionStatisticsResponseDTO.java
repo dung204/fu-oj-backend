bfd74c586cf703d1c4875b48588b143b7e1823b4
dung204 <acezombiev4@gmail.com>
2025-10-28 12:25:13 +0700

    feat: add submission statistics endpoint

---
+++ src/main/java/com/example/modules/submissions/dtos/SubmissionStatisticsResponseDTO.java
@@ -0,0 +1,36 @@
+package com.example.modules.submissions.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SubmissionStatisticsResponseDTO {
+
+  @JsonProperty("ACCEPTED")
+  private Long accepted;
+
+  @JsonProperty("WRONG_ANSWER")
+  private Long wrongAnswer;
+
+  @JsonProperty("TIME_LIMIT_EXCEEDED")
+  private Long timeLimitExceeded;
+
+  @JsonProperty("COMPILATION_ERROR")
+  private Long compilationError;
+
+  @JsonProperty("RUNTIME_ERROR")
+  private Long runtimeError;
+
+  @JsonProperty("MEMORY_LIMIT_EXCEEDED")
+  private Long memoryLimitExceeded;
+
+  @JsonProperty("RETURN_ERROR")
+  private Long returnError;
+
+  @JsonProperty("INVALID_RETURN")
+  private Long invalidReturn;
+
+  private Long totalCount;
+}
