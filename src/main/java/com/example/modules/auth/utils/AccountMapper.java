d87770ea7caceb18d05747ba97f4761dce97eb7d
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2025-11-10 22:04:10 +0700

    fix: fix data response in accountResponseDTO

--- src/main/java/com/example/modules/auth/utils/AccountMapper.java
+++ src/main/java/com/example/modules/auth/utils/AccountMapper.java
@@ -4,10 +4,15 @@ import com.example.modules.auth.dtos.account.AccountResponseDTO;
 import com.example.modules.auth.entities.Account;
 import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 @Slf4j
 public abstract class AccountMapper {
 
+  @Mapping(
+    target = "role",
+    expression = "java(account.getRole() != null ? account.getRole().getValue() : null)"
+  )
   public abstract AccountResponseDTO toAccountResponseDTO(Account account);
 }
