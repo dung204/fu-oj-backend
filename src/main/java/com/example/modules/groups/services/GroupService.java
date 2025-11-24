72e50b691d3ca64f1f6e68f22233fc6cf588ce78
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2025-11-24 22:01:25 +0700

    feat: fix bug display private group after join

--- src/main/java/com/example/modules/groups/services/GroupService.java
+++ src/main/java/com/example/modules/groups/services/GroupService.java
@@ -96,6 +96,7 @@ public class GroupService {
           GroupsSpecification.builder()
             .containsName(groupsSearchDTO.getName())
             .orderByCreatedTimestampDesc()
+            .joinedBy(currentUser.getId())
             .<GroupsSpecification>conditionally(
               "joined".equals(groupsSearchDTO.getFilter()),
               spec -> spec.joinedBy(currentUser.getId())
