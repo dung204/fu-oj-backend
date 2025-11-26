48cb5bff5702dd4192096274003ae5ec76a3fe4d
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2025-11-26 21:32:23 +0700

    feat: refactor CommentsService formatting and add Lombok to CommentEvent

--- src/main/java/com/example/modules/redis/event_type/comment/CommentEvent.java
+++ src/main/java/com/example/modules/redis/event_type/comment/CommentEvent.java
@@ -1,11 +1,15 @@
 package com.example.modules.redis.event_type.comment;
 
 import com.example.modules.comments.dtos.CommentResponseDTO;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentEvent {
 
   private CommentEventType type;
